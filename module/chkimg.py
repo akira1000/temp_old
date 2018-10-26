fc5067196616b6ac8edb2f2756b1586b726d66e2
Akira S <mxa03353@nifty.com>
2018-10-26 17:15:29 +0900

    imgディレクトリを無くす。日本語ファイルを扱えるようにする

--- module/chkimg.py
+++ module/chkimg.py
@@ -4,32 +4,45 @@ import numpy as np
 from PIL import ImageGrab
 
 
+#OpenCVのcv2.imreadは日本語がつかえないなので画像をimedecodeでメモリに一度読み込む
+def imread(filename, flags=cv2.IMREAD_COLOR, dtype=np.uint8):
+    try:
+        n = np.fromfile(filename, dtype)
+        img2 = cv2.imdecode(n, flags)
+        return img2
+    except Exception as e:
+        print(e)
+        return None
+
+
 def chkimg(image="mark.bmp",imgminx=0,imgminy=0,imgmaxx=0,imgmaxy=0,threshold=0.8):
+	if imgmaxx != 0:
+		imgtemp = ImageGrab.grab((imgminx, imgminy, imgmaxx, imgmaxy))
+	if imgmaxx == 0:
+		imgtemp = ImageGrab.grab()
+	Screencap = np.asarray(imgtemp)
 
+	# 画像をグレースケールで読み込む
+	Screenimg = cv2.cvtColor(Screencap, cv2.COLOR_BGR2GRAY)
 
-		if imgmaxx != 0:
-			imgtemp=ImageGrab.grab((imgminx,imgminy,imgmaxx,imgmaxy))
-		if imgmaxx == 0:
-			imgtemp=ImageGrab.grab()
-		cap = np.asarray(imgtemp)
-		#img.save("img2.bmp")
+	# 日本語試行錯誤中
+	# この下で動く
+	finalname = image
 
-		#画像をグレースケールで読み込む
-		img = cv2.cvtColor(cap, cv2.COLOR_BGR2GRAY)
-		temp = cv2.imread("img\\"+image, 0)
-		#マッチングテンプレートを実行
-		result = cv2.matchTemplate(img, temp, cv2.TM_CCOEFF_NORMED)
-		#類似度の設定(0~1)
-		#threshold = 0.9
-		#検出結果から検出領域の位置を取得
+	#		temp = cv2.imread(finalname, 0)
+	temp = imread(finalname, 0)
+
+	# マッチングテンプレートを実行
+	result = cv2.matchTemplate(Screenimg, temp, cv2.TM_CCOEFF_NORMED)
+	# threshold = 0.9
 	#    loc = np.where(result >= threshold)
 
-		min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(result)
-		#max_valが類似最大値の左上座標
+	min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(result)
+	# max_valが類似最大値の左上座標
 
-		if max_val < threshold:
-			print(image+"一致せず")
+	if max_val < threshold:
+			print(image+" 一致せず")
 			return (0)
-		if max_val >= threshold:
+	if max_val >= threshold:
 			print(max_loc)
 			return (max_loc)
